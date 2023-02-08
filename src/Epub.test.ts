0d3edf23563a31144f5a1c9ad6094f3bb0b97356
bmaupin <bmaupin@users.noreply.github.com>
2023-02-08 13:38:42 -0500

    feat: Add write() method
    ... and add mimetype file

--- src/Epub.test.ts
+++ src/Epub.test.ts
@@ -1,10 +1,25 @@
-import { describe, expect, test } from 'vitest';
+import { writeFile } from 'fs/promises';
+import { beforeAll, describe, expect, test } from 'vitest';
 
 import Epub from './Epub';
 
+let epub: Epub;
+
 describe('Epub', () => {
+  beforeAll(() => {
+    epub = new Epub({ id: '38e9a65c-8077-45b7-a59e-8d0ae827ca5f' });
+  });
+
   test('Create new Epub', () => {
-    const epub = new Epub({ id: '38e9a65c-8077-45b7-a59e-8d0ae827ca5f' });
     expect(epub).toBeInstanceOf(Epub);
   });
+
+  test('Write Epub', async () => {
+    await writeFile(
+      'test.epub',
+      Buffer.from(await (await epub.write()).arrayBuffer())
+    );
+  });
 });
+
+// TODO: add test(s) to make sure EPUB is valid
--- src/Epub.ts
+++ src/Epub.ts
@@ -1,7 +1,43 @@
+import { BlobWriter, TextReader, ZipWriter } from '@zip.js/zip.js';
+
 interface EpubOptions {
   id: string;
 }
 
 export default class Epub {
-  constructor(options: EpubOptions) {}
+  constructor(options: EpubOptions) {
+    // TODO
+  }
+
+  async write(): Promise<Blob> {
+    const zipFileWriter = new BlobWriter();
+
+    // As per the EPUB spec, the mimetype file must come first and must be uncompressed
+    const uncompressedZipWriter = new ZipWriter(zipFileWriter, { level: 0 });
+    await uncompressedZipWriter.add(
+      'mimetype',
+      new TextReader('application/epub+zip')
+    );
+    await uncompressedZipWriter.close();
+
+    return await zipFileWriter.getData();
+  }
 }
+
+/*
+ * TODO:
+ *
+ * 1. EPUB 3 MVP
+ *   1. Create minimal package.opf file
+ *   1. Create minimal nav.xhtml
+ *   1. Package it up (zip) with mimetype and container.xml files
+ *   1. Implement addSection
+ *   1. Get validation test working
+ * 1. Set up GitHub Actions
+ * 1. Add EPUBv2 TOC
+ * 1. Start adding features
+ *    1. Default CSS
+ *    1. Exclude section from TOC
+ *    1. Cover
+ * 1. Add tsdoc
+ */
