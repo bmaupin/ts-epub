a741476f539f1b0dc31ad2974a27f6d152ead2b7
bmaupin <bmaupin@users.noreply.github.com>
2023-02-08 13:15:05 -0500

    chore: Enable no-floating-promises rule
    Forgetting to add await always causes me a lot of problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,18 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   ignorePatterns: ['/types'],
   overrides: [
+    // Enable @typescript-eslint/no-floating-promises only for TypeScript files; it
+    // requires parserOptions.project which causses errors with this specific file:
+    // "error  Parsing error: ESLint was configured to run on `<tsconfigRootDir>/.eslintrc.js` using `parserOptions.project`: <tsconfigRootDir>/tsconfig.json
+    // However, that TSConfig does not include this file."
+    {
+      files: ['*.ts'],
+      parserOptions: { project: './tsconfig.json' },
+      rules: {
+        '@typescript-eslint/no-floating-promises': ['error'],
+      },
+    },
+    // Add an override to allow Node.js imports in tests
     {
       files: ['*.test.ts'],
       rules: {
@@ -22,6 +34,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   root: true,
   rules: {
+    // Prevent importing Node.js libraries since this library should also work in the browser
     'no-restricted-imports': [
       'error',
       'assert',
