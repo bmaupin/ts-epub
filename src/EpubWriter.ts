3b1af43b01a5059f4fa10eaa84d40284ca39afae
bmaupin <bmaupin@users.noreply.github.com>
2023-02-16 13:32:24 -0500

    feat: Prettify XML
    NOTE this doesn't actually work. Neither Node nor jsdom has XSLTProcessor. Plus it sounds like it might be pretty browser dependent too. I think we'll instead need to find a small library that can do this

--- src/EpubWriter.ts
+++ src/EpubWriter.ts
@@ -46,12 +46,42 @@ export default class EpubWriter {
       <rootfiles>
         <rootfile full-path="EPUB/package.opf" media-type="application/oebps-package+xml" />
       </rootfiles>
-    </container>\n`;
+    </container>`;
 
     await zipWriter.add(
       'META-INF/container.xml',
-      new TextReader(containerXmlContent)
+      new TextReader(EpubWriter.prettifyXml(containerXmlContent))
     );
+  }
+
+  // https://stackoverflow.com/a/47317538/399105
+  static prettifyXml(sourceXml: string): string {
+    const xmlDoc = new DOMParser().parseFromString(
+      sourceXml,
+      'application/xml'
+    );
+    const xsltDoc = new DOMParser().parseFromString(
+      [
+        // describes how we want to modify the XML - indent everything
+        '<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform">',
+        '  <xsl:strip-space elements="*"/>',
+        '  <xsl:template match="para[content-style][not(text())]">', // change to just text() to strip space in text nodes
+        '    <xsl:value-of select="normalize-space(.)"/>',
+        '  </xsl:template>',
+        '  <xsl:template match="node()|@*">',
+        '    <xsl:copy><xsl:apply-templates select="node()|@*"/></xsl:copy>',
+        '  </xsl:template>',
+        '  <xsl:output indent="yes"/>',
+        '</xsl:stylesheet>',
+      ].join('\n'),
+      'application/xml'
+    );
+
+    const xsltProcessor = new XSLTProcessor();
+    xsltProcessor.importStylesheet(xsltDoc);
+    const resultDoc = xsltProcessor.transformToDocument(xmlDoc);
+    const resultXml = new XMLSerializer().serializeToString(resultDoc);
+    return resultXml;
   }
 
   private async writePackageOpf(zipWriter: ZipWriter<Blob>): Promise<void> {
